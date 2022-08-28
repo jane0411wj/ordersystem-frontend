38d0764f6c704cf2089567ed1e0c056acc209df2
xcf <973345277@qq.com>
2022-08-28 21:59:01 +0800

    添加公用请求

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
-import {
-	$http
-} from '@escook/request-miniprogram'
+import { $http } from '@escook/request-miniprogram'
 Vue.config.productionTip = false
 uni.$http = $http
 App.mpType = 'app'
-
+$http.baseUrl = 'https://www.example.com'
+$http.beforeRequest = function (options) {
+  wx.showLoading({
+    title: '数据加载中...',
+  })
+}
+// 请求开始之前做一些事情
+$http.beforeRequest = function (options) {
+  if (options.url.indexOf('/home/catitems') !== -1) {
+    options.header = {
+      'X-Test': 'AAA',
+    }
+  }
+}
+// 请求完成之后做一些事情
+$http.afterRequest = function () {
+  // do something...
+}
 const app = new Vue({
 	...App
 })
